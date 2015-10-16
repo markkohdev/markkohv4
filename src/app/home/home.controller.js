5fa5b1718e7eb525b86d24272a0bd4aa04b44336
Mark Koh <mkoh@rjmetrics.com>
2015-10-16 16:45:43 +0000

    Added pages and routing.

---
+++ src/app/home/home.controller.js
@@ -0,0 +1,13 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('markkoh')
+    .controller('HomeController', HomeController);
+
+  /** @ngInject */
+  function HomeController() {
+    // var vm = this;
+
+  }
+})();
--- src/app/index.route.js
+++ src/app/index.route.js
@@ -8,12 +8,41 @@
   /** @ngInject */
   function routerConfig($stateProvider, $urlRouterProvider) {
     $stateProvider
-      .state('home', {
+      .state('default', {
+        templateUrl: 'app/main/main.html'
+      })
+      // Nest home into default
+      .state('default.home', {
         url: '/',
-        templateUrl: 'app/main/main.html',
-        controller: 'MainController',
-        controllerAs: 'main'
-      });
+        templateUrl: 'app/home/home.html',
+        controller: 'HomeController',
+        controllerAs: 'vm'
+      })
+      .state('default.about', {
+        url: '/about',
+        templateUrl: 'app/about/about.html',
+        controller: 'AboutController',
+        controllerAs: 'vm'
+      })
+      .state('default.portfolio', {
+        url: '/portfolio',
+        templateUrl: 'app/portfolio/portfolio.html',
+        controller: 'PortfolioController',
+        controllerAs: 'vm'
+      })
+      .state('default.photography', {
+        url: '/photography',
+        templateUrl: 'app/photography/photography.html',
+        controller: 'PhotographyController',
+        controllerAs: 'vm'
+      })
+      .state('default.contact', {
+        url: '/contact',
+        templateUrl: 'app/contact/contact.html',
+        controller: 'ContactController',
+        controllerAs: 'vm'
+      })
+      ;
 
     $urlRouterProvider.otherwise('/');
   }
